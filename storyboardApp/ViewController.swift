62b94bd592f652f2708a2d3cebf81aa3e7231f6d
kevin31613 <user_14@mac-14deimac.local>
2017-04-06 09:34:44 +0800

    Initial Commit

---
+++ storyboardApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  storyboardApp
+//
+//  Created by user_14 on 2017/4/6.
+//  Copyright © 2017年 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
